ca7cb97826aecf96b30b4d7fd0530321c10b2a79
Anna <ankamorozka@outlook.com>
2024-05-22 23:39:31 +0300

    ver 2

--- src/repository/images.py
+++ src/repository/images.py
@@ -3,18 +3,48 @@ from typing import List
 from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
+from src.models.models import Image, User, Tag
+from src.schemas.schemas import ImageModel, ImageUpdate
 
-
-from src.models.models import Image, User
-
-def get_all_images(skip: int, limit: int, db: Session) -> List[Image]:
+async def get_all_images(skip: int, limit: int, db: Session) -> List[Image]:
     return db.query(Image).offset(skip).limit(limit).all()
 
-def get_images_by_user(user_id: int, db: Session) -> List[Image]:
+async def get_images_by_user(user_id: int, db: Session) -> List[Image]:
     user = db.query(User).filter(User.id == user_id).first()
     return db.query(Image).filter(Image.user_id == user.id).all()
 
-def get_images_by_id(image_id: int, db: Session) -> Image:
+async def get_images_by_id(image_id: int, db: Session) -> Image:
     return db.query(Image).filter(Image.id == image_id).all()
 
+async def create_image(body: ImageModel, db: Session) -> Image:
 
+        #, user: User,
+
+    user = db.query(User).filter(User.id == 1).first()
+    tags = db.query(Tag).filter(Tag.id.in_(body.tags)).all()
+    image = Image(image=body.image, qr_code=body.qr_code, user_id=user.id,
+                      description=body.description, tags=tags)
+
+    db.add(image)
+    db.commit()
+    db.refresh(image)
+    return image
+
+async def remove_image(image_id: int, db: Session,  user: int) -> Image | None:
+    image = db.query(Image).filter(and_(Image.id == image_id, Image.user_id == user)).first()
+    if image:
+        db.delete(image)
+        db.commit()
+    return image
+
+
+async def update_image(image_id: int, body: ImageUpdate, db: Session, user: int) -> Image | None:
+    exist_image = db.query(Image).filter(and_(Image.id == image_id, Image.user_id == user)).first()
+    if exist_image:
+        tags = db.query(Tag).filter(Tag.id.in_(body.tags)).all()
+        exist_image.qr_code = body.qr_code
+        exist_image.description = body.description
+        exist_image.edited_image = body.edited_image
+        exist_image.tags = tags
+        db.commit()
+    return exist_image
---
+++ src/schemas/schemas.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
+class UserForImage(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        from_attributes = True
+
+class CommentBase(BaseModel):
+    comment: str
+
+class CommentResponse(CommentBase):
+    id: int
+    user: UserForImage
+
+    class Config:
+        from_attributes = True
+
+
+class TagModel(BaseModel):
+    name: str = Field(max_length=25)
+
+
+class TagResponse(TagModel):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class ImageBase(BaseModel):
+    image: str = Field(max_length=255)
+    description: str = Field(max_length=100)
+    qr_code: str = Field(max_length=255)
+    user_id: int
+
+class ImageModel(ImageBase):
+    tags: List[TagResponse]
+    comments: Optional[List[CommentResponse]]
+
+class ImageUpdate(BaseModel):
+    description: str = Field(max_length=100)
+    qr_code: str = Field(max_length=255)
+    edited_image: str = Field(max_length=255)
+    tags: List[int]
+    comments: Optional[List[int]]
+
+    class Config:
+        from_attributes = True
+
+class ImageResponse(ImageBase):
+    id: int
+    user: UserForImage
+    tags: List[TagResponse]
+    comments: Optional[List[CommentResponse]]
+
+    class Config:
+        from_attributes = True
+
