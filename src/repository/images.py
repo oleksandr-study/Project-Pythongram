06025f70dc9aa4fde90d5e471155a77aaa8c6c4d
Kollos777 <kolloslit@gmail.com>
2024-05-23 11:55:28 -0500

    Add tag handling V2

--- src/repository/images.py
+++ src/repository/images.py
@@ -2,7 +2,7 @@ from typing import List
 
 from sqlalchemy import and_
 from sqlalchemy.orm import Session
-
+from fastapi import APIRouter, HTTPException, Depends, status
 from src.models.models import Image, User, Tag
 from src.schemas.schemas import ImageModel, ImageUpdate
 
@@ -16,14 +16,26 @@ async def get_images_by_user(user_id: int, db: Session) -> List[Image]:
 async def get_images_by_id(image_id: int, db: Session) -> Image:
     return db.query(Image).filter(Image.id == image_id).all()
 
-async def create_image(body: ImageModel, db: Session) -> Image:
-
-        #, user: User,
-
+async def create_image(body: ImageModel, db: Session,#user: User
+                       ) -> Image:
+    if len(body.tags) > 5:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You can add up to 5 tags only.")
+    tags = []
+    for tag_name in body.tags:
+        tag = db.query(Tag).filter(Tag.name == tag_name).first()
+        if not tag:
+            tag = Tag(name=tag_name)
+            db.add(tag)
+            db.commit()
+        tags.append(tag)
     user = db.query(User).filter(User.id == 1).first()
-    tags = db.query(Tag).filter(Tag.id.in_(body.tags)).all()
-    image = Image(image=body.image, qr_code=body.qr_code, user_id=user.id,
-                      description=body.description, tags=tags)
+    image = Image(
+            image=body.image,
+            qr_code=body.qr_code,
+            user_id=user.id,
+            description=body.description,
+            tags=tags
+        )
 
     db.add(image)
     db.commit()
@@ -41,7 +53,17 @@ async def remove_image(image_id: int, db: Session,  user: int) -> Image | None:
 async def update_image(image_id: int, body: ImageUpdate, db: Session, user: int) -> Image | None:
     exist_image = db.query(Image).filter(and_(Image.id == image_id, Image.user_id == user)).first()
     if exist_image:
-        tags = db.query(Tag).filter(Tag.id.in_(body.tags)).all()
+        if len(body.tags) > 5:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You can add up to 5 tags only.")
+        tags = []
+        for tag_name in body.tags:
+            tag = db.query(Tag).filter(Tag.name == tag_name).first()
+            if not tag:
+                tag = Tag(name=tag_name)
+                db.add(tag)
+                db.commit()
+            tags.append(tag)
+
         exist_image.qr_code = body.qr_code
         exist_image.description = body.description
         exist_image.edited_image = body.edited_image
