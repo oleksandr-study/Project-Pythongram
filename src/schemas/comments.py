934831e2c91e63de68206ddf25c87ace54e13290
oleksandr-study <oleksandr.study@protonmail.com>
2024-05-25 11:00:28 +0100

    fix ImportError: cannot import name 'UserForImage'

--- src/schemas/comments.py
+++ src/schemas/comments.py
@@ -2,6 +2,15 @@ from pydantic import BaseModel, Field
 from src.schemas.images import UserForImage
 
 
+class UserForImage(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        from_attributes = True
+        
+
 class CommentBase(BaseModel):
     comment: str
 
--- src/schemas/images.py
+++ src/schemas/images.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel, Field
-from src.schemas.comments import CommentResponse
-from src.schemas.tags import TagResponse
-
 
 class UserForImage(BaseModel):
     id: int
@@ -13,6 +10,26 @@ class UserForImage(BaseModel):
     class Config:
         from_attributes = True
 
+class CommentBase(BaseModel):
+    comment: str
+
+class CommentResponse(CommentBase):
+    id: int
+    user: UserForImage
+
+    class Config:
+        from_attributes = True
+
+
+class TagModel(BaseModel):
+    name: str = Field(max_length=25)
+
+
+class TagResponse(TagModel):
+    id: int
+
+    class Config:
+        from_attributes = True
 
 class ImageBase(BaseModel):
     image: str = Field(max_length=255)
