6eed9f3a30f469fa9a0d2230b65315e434607891
Bohdan Datsko <bohdandatsko5@gmail.com>
2024-05-25 18:03:34 +0300

    new comments

--- src/routes/comments.py
+++ src/routes/comments.py
@@ -18,14 +18,14 @@ async def get_comments(image_id: int,db: Session = Depends(get_db)): #,current_u
 async def create_comment(image_id: int, body: CommentBase, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     return await comments.create_comment(image_id, body, db, current_user.id)
 
-@router.patch('/{image_id}/comments/{comment_id}/', response_model=CommentResponse)
+@router.patch('/comments/{comment_id}/', response_model=CommentResponse)
 async def update_comm(comment_id: int, body: CommentBase, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     comment = await comments.update_comment(db,body,user_id=current_user.id, comment_id=comment_id)
     if comment is None:
          raise HTTPException(status_code=404, detail="Comment not found")
     return comment
 
-@router.delete('/{image_id}/comments/{comment_id}/', response_model=CommentResponse)
+@router.delete('/comments/{comment_id}/', response_model=CommentResponse)
 async def delete_comment(comment_id: int, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     if current_user.role not in [Role.admin, Role.moderator]:
         raise HTTPException(status_code=403, detail="Not enough permissions")
