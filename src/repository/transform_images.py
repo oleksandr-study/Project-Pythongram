6243557ac3526b073f3215e8ea6213948f147902
oleksandr-study <oleksandr.study@protonmail.com>
2024-05-26 20:11:19 +0100

    add qr code func

--- src/repository/transform_images.py
+++ src/repository/transform_images.py
@@ -1,6 +1,43 @@
+import cloudinary
+import cloudinary.uploader
+import qrcode
+
 from typing import Dict
 from fastapi import HTTPException
+from pathlib import Path
+from sqlalchemy.orm import Session
 from cloudinary.utils import cloudinary_url
+
+from src.conf.config import cloudinary_start
+
+
+def make_qr_code(edited_image_url: Path, image_id: int, db: Session):
+    import qrcode
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data('Some data')
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    
+    qr_code_file = "qr_code.png"
+    img.save(qr_code_file)
+
+    cloudinary_start()
+    upload_qr_code = cloudinary.uploader.upload(
+        qr_code_file,
+        public_id=f"Qr_Code/qr_code_{image_id}",
+        overwrite=True,
+        invalidate=True,
+    )
+
+    return upload_qr_code["secure_url"]
+
 
 async def get_transformed_image_url(public_id: str, transformations: Dict) -> str:
     """
